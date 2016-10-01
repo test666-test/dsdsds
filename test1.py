107392e1970edab7d30d794dfc75ab6cd354ba6a
test666-test <czupaczups666@gmail.com>
2016-10-01 19:19:55 +0200

    Update test1.py

--- test1.py
+++ test1.py
@@ -3,12 +3,24 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.keys import Keys
 import time
-
-def fun_1(unittest.TestCase):
-	driver = webdriver.Chrome()
-	driver.get("http://store.steampowered.com/")
-	x= driver.find_element_by_name("term")
-	x_send.keys(Keys.RETURN)
-	time.sleep(3)
+import sys
 
 
+class test_1(unittest.TestCase):
+    
+        driver = webdriver.Chrome()
+        driver.get("http://www.google.pl")
+        print(driver.title)
+        x=driver.find_elements_by_css_selector(input[id^='g'])
+        print(x)
+        x.send_keys("kutas")
+        x.send_keys(Keys.RETURN)
+        
+
+        x= driver.find_element_by_css_selector("#tsf > div.tsf-p > div.jsb > center > input[type='submit']:nth-child(1)")
+
+        x.click()
+        x.submit()
+        time.sleep(3)
+        assert "Tibia" in driver.page_source
+unittest.main()
